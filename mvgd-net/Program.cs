842f64f83a0ae11ff43dc9d11d9b463d136b455b
Bonny1992 <bonny1992@gmail.com>
2024-09-21 23:50:19 +0200

    Changed destination folder

--- mvgd-net/Program.cs
+++ mvgd-net/Program.cs
@@ -36,7 +36,7 @@ class Program
             if (!string.IsNullOrEmpty(selectedFile))
             {
                 char firstLetter = char.ToUpper(selectedFile[0]);
-                string destinationFolder = Path.Combine(@"/mnt/addons/merged-remotes/dropbox-crypt/Home/Other games", $"{firstLetter}");
+                string destinationFolder = Path.Combine(@"/mnt/addons/merged-remotes/td-personal-bonny-home-union-crypt/Home/Other games", $"{firstLetter}");
                 
                 if (!Directory.Exists(destinationFolder))
                 {
